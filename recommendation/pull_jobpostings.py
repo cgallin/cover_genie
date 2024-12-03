f0d0d804ca8e126ebd0374be23b03618726b184f
Arnav Roy <arnavmenonroy@gmail.com>
2024-12-03 17:04:21 -0500

    adding data pickle file for montreal and updating jobpostings accordingly

--- recommendation/pull_jobpostings.py
+++ recommendation/pull_jobpostings.py
@@ -1,4 +1,7 @@
 import pickle
 
 with open("api_data/data_toronto_jobs.pkl", "rb") as file:
-    df = pickle.load(file)
+    df_toronto = pickle.load(file)
+
+with open("api_data/data_montreal_jobs.pkl", "rb") as file:
+    df_montreal = pickle.load(file)
