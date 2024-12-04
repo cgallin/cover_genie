8d18360b5f5d0e7f6b94804d0001cd68eef5bf66
julia greenwood <j.greenwood98@outlook.com>
2024-12-04 14:56:02 -0500

    setup.py +fast_api

--- cg_prod/cg_interface/app.py
+++ cg_prod/cg_interface/app.py
@@ -11,42 +11,6 @@ st.markdown(''' # Automate your job search: Generate Cover letters with ease!'''
 
 st.markdown(''' Please enter the following information to generate relevant job postings:''')
 
-url = 'https://X.X.X/recommend'
-
-if st.button('Recommend jobs'):
-    pred = requests.get(url, params=query_params).json()
-    # pred["fare"] = pred["fare"]
-    # st.markdown(f'This ride will cost you: ${pred["fare"]}')
-
-# Get input function
-def input():
-    job_title_1 = st.text_input('Enter a desired job title: '),
-    job_title_2 = st.text_input('Enter a desired job title: '),
-    job_title_3 = st.text_input('Enter a desired job title: '),
-
-    location = st.text_input('Enter the desired work location: '),
-
-    user_cv = st.file_uploader('Upload your CV in PDF format: ', type=['pdf'], accept_multiple_files=False),
-
-    if user_cv is not None:
-        # To read file as bytes:
-        bytes_data = user_cv.getvalue() # not sure about this bit, how to make it work with the pdf text extractor
-        st.write('filename:', user_cv.name)
-
-    industries = get_select_industries()
-
-    # Getting params to generate job recommendations,
-    query_params = {
-        'job_title_1': job_title_1,
-        'job_title_2': job_title_2,
-        'job_title_3': job_title_3,
-        'location': location,
-        'user_cv': bytes_data,
-        'industries': industries,
-    }
-    return query_params
-
-# Selecting Job industry
 
 industries = ['Healthcare and Biotechnology',
             'Technology',
@@ -68,7 +32,7 @@ industries = ['Healthcare and Biotechnology',
             'Environmental and Renewable Energy',
             'Utilities']
 
-
+# Selecting Job industry
 def get_select_industries():
     st.write('Select the relevant industries for your job search: ')
 
@@ -82,14 +46,58 @@ def get_select_industries():
     st.write('You selected:', selected_industries)
     return selected_industries
 
-# Output from recommendation system: displaying job recommendations in a dataframe.
+# Get input function
+def input():
+    job_title_1 = st.text_input('Enter a first desired job title: '),
+    job_title_2 = st.text_input('Enter a second desired job title: '),
+    job_title_3 = st.text_input('Enter a third desired job title: '),
 
-def get_dataframe_data():
+    location = st.multiselect(
+     'Enter the desired work location: ',
+     ['Montreal', 'Toronto'])
 
-    return pd.DataFrame(
-# data frame with job recommendations: Job title, company, job description, apply button
-        )
+    user_cv = st.file_uploader('Upload your CV in PDF format: ', type=['pdf'], accept_multiple_files=False),
 
-df = get_dataframe_data()
+    if user_cv is not None:
+        # To read file as bytes:
+        bytes_data = user_cv.getvalue() # not sure about this bit, how to make it work with the pdf text extractor
+        st.write('filename:', user_cv.name)
 
-# Generate cover letter page
+    industries = get_select_industries()
+
+    # Getting params to generate job recommendations,
+    query_params = {
+        'job_title_1': job_title_1,
+        'job_title_2': job_title_2,
+        'job_title_3': job_title_3,
+        'location': location,
+        'user_cv': bytes_data,
+        'industries': industries,
+    }
+    st.write(query_params)
+    return query_params
+
+url = ''
+params = input()
+
+
+
+# if st.button('Recommend jobs'):
+#     pred = requests.get(url, params=query_params).json()
+#     # pred["fare"] = pred["fare"]
+#     # st.markdown(f'This ride will cost you: ${pred["fare"]}')
+
+
+
+
+# # Output from recommendation system: displaying job recommendations in a dataframe.
+
+# def get_dataframe_data():
+
+#     return pd.DataFrame(
+# # data frame with job recommendations: Job title, company, job description, apply button
+#         )
+
+# df = get_dataframe_data()
+
+# # Generate cover letter page
