1c142c5340074782d5764c2d50d34e0b2701e6aa
Cameron Gallinger <cam.gallinger@gmail.com>
2024-12-05 16:30:00 -0500

    push

--- api_data/predicted_df.py
+++ api_data/predicted_df.py
@@ -1,4 +1,13 @@
 import pickle
+import pandas as pd
+import bert.bert_model as bert_model
 
-with open("api_data/data_toronto_jobs.pkl", "rb") as file:
-    df_toronto = pickle.load(file)
+file = open("data_vancouver_jobs.pkl", "rb")
+van_df=pickle.load(file)
+
+def predict_industry_df(df):
+    df["description_cleaned"]=df["description"].apply(bert_model.clean_text)
+    df["industry"]=df["description_cleaned"].apply(bert_model.predict_industry)
+    return df
+
+predict_industry_df(van_df)
