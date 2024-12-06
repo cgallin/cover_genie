cc87f8f646f3b4c45b3e55ff7cc331c073e36756
Cameron Gallinger <cam.gallinger@gmail.com>
2024-12-06 18:11:22 -0500

    eod push

--- bert/params.py
+++ bert/params.py
@@ -1,12 +1,24 @@
 
-SEQUENCE_LENGTH = 256
+SEQUENCE_LENGTH = 512
 BATCH_SIZE = 32
 TRAIN_SPLIT = 0.7
-VAL_SPLIT = 0.4
-EPOCHS = 2
-LEARNING_RATE = 2e-5
+VAL_SPLIT = 0.2
+EPOCHS = 1
+LEARNING_RATE = 2e-6
 PRE_TRAINED_MODEL_NAME = "distil_bert_base_en_uncased"
 PREPROCESSOR_NAME = "distil_bert_base_en"
 NUM_CLASSES = 11
-SAMPLE_FRAC = 0.1
+SAMPLE_FRAC = 0.5
 DROPOUT_RATE = 0.2
+CLASS_WEIGHTS = {0:1.92971879,
+                 1:0.93211113,
+                 2:1.84661667,
+                 3:0.72253217,
+                 4:0.4758921 ,
+                5:1.67547424,
+                6:3.06134997,
+                7:0.66806478,
+                8:1.47934985,
+                9:0.49944457,
+                10:3.52695789}
+TRAINABLE = True
